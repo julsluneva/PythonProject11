545763aef49a4c3fca56233194783e02899ab0e9
Yulia Luneva <julsluneva@gmail.com>
2025-08-23 13:54:49 +0300

    tests utils done

--- src/external_api.py
+++ src/external_api.py
@@ -1,12 +1,14 @@
 import os
+from typing import Any, Dict
+
 import requests
-from typing import Dict, Any
 from dotenv import load_dotenv
 
 load_dotenv()
-cached_rates = {}
+cached_rates: dict[str, float] = {}
 
-def get_exchange_rate(from_currency: str, to_currency: str = 'RUB') -> float:
+
+def get_exchange_rate(from_currency: str, to_currency: str = "RUB") -> float:
     """Получает текущий курс валюты к рублю(по умолчанию)"""
     # Проверяем, есть ли курс в кэше
     cache_key = f"{from_currency}_{to_currency}"
@@ -14,7 +16,7 @@ def get_exchange_rate(from_currency: str, to_currency: str = 'RUB') -> float:
         return cached_rates[cache_key]
 
     # Получаем API ключ из переменной окружения
-    api_key = os.getenv('API_KEY')
+    api_key = os.getenv("API_KEY")
     if not api_key:
         raise ValueError("API key not found in environment variables")
 
@@ -28,12 +30,12 @@ def get_exchange_rate(from_currency: str, to_currency: str = 'RUB') -> float:
 
         # Проверка статуса
         if response.status_code != 200:
-            raise Exception(f"API error: {data.get('error',{}).get('info', 'Unknown error')}")
+            raise Exception(f"API error: Status code {response.status_code}")
 
         data = response.json()
 
         # Сохраняем курс в кэш
-        exchange_rate = data['rates'][to_currency]
+        exchange_rate = data["rates"][to_currency]
         cached_rates[cache_key] = exchange_rate
         return exchange_rate
 
@@ -42,19 +44,20 @@ def get_exchange_rate(from_currency: str, to_currency: str = 'RUB') -> float:
     except KeyError as e:
         raise Exception(f"Invalid response format: {e}")
 
+
 def convert_currency(transaction_data: Dict[str, Any]) -> float:
     """Конвертирует сумму транзакции в рубли(float)"""
     try:
         # Извлекаем данные из транзакции
-        amount_str = transaction_data['operationAmount']['amount']
-        currency_code = transaction_data['operationAmount']['currency']['code']
+        amount_str = transaction_data["operationAmount"]["amount"]
+        currency_code = transaction_data["operationAmount"]["currency"]["code"]
         amount = float(amount_str)
 
         # Если валюта уже в рублях, то возвращаем, как есть
-        if currency_code == 'RUB':
+        if currency_code == "RUB":
             return amount
         # Конвертируем USD и EUR в рубли
-        if currency_code in ['USD', 'EUR']:
+        if currency_code in ["USD", "EUR"]:
             exchange_rate = get_exchange_rate(currency_code)
             return amount * exchange_rate
         return amount
@@ -65,5 +68,3 @@ def convert_currency(transaction_data: Dict[str, Any]) -> float:
         raise ValueError(f"Invalid amount format: {e}")
     except Exception as e:
         raise Exception(f"Currency conversion failed: {e}")
-
-
--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,18 @@
 import json
 import os
-from typing import List, Dict, Any
+from typing import Any, Dict, List
+
 
 def read_operations_json() -> List[Dict[Any, Any]]:
     """Функция для чтения json-файла из директории data в папке operation.json"""
 
-    file_path = os.path.join('..', '..', 'data', 'operation.json')
+    file_path = os.path.join("..", "..", "data", "operation.json")
 
     try:
         if not os.path.isfile(file_path):
             return []
 
-        with open(file_path, 'r', encoding='utf-8') as file:
+        with open(file_path, "r", encoding="utf-8") as file:
             data = json.load(file)
 
             if isinstance(data, list) and all(isinstance(item, dict) for item in data):
